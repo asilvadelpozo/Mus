4ea9aa3282b1e3aacdf1dc5b33890c84788fe110
Antonio Silva del Pozo <asilvadelpozo@gmail.com>
2015-08-12 02:38:38 +0200

    Add bubble chat

--- mus_client/dist/js/main.js
+++ mus_client/dist/js/main.js
@@ -361,6 +361,9 @@
             },
             controller: ['$scope', function ($scope) {
                 $scope.getPlayerIndexClass = function() {
+                    if($scope.players.indexOf($scope.message.playerName) === -1) {
+                        return 'message__player--room';
+                    }
                     return 'message__player--' + $scope.players.indexOf($scope.message.playerName);
                 };
             }]

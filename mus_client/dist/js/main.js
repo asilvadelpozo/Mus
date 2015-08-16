cac3bbc2dcbed3d263f537b5c6b8e9d4daea13cb
Antonio Silva del Pozo <asilvadelpozo@gmail.com>
2015-08-16 14:04:10 +0200

    Fix card sizes for all viewports

--- mus_client/dist/js/main.js
+++ mus_client/dist/js/main.js
@@ -5,23 +5,6 @@
 
 })();
 
-(function() {
-    'use strict';
-
-    angular.module('musApp')
-        .filter('withoutNull', function() {
-            return function(input) {
-                var result = [];
-                input.forEach(function(entry) {
-                    if(entry !== null) {
-                        result.push(entry);
-                    }
-                });
-                return result;
-            };
-        });
-})();
-
 (function () {
     'use strict';
 
@@ -403,6 +386,23 @@
         };
     });
 })();
+(function() {
+    'use strict';
+
+    angular.module('musApp')
+        .filter('withoutNull', function() {
+            return function(input) {
+                var result = [];
+                input.forEach(function(entry) {
+                    if(entry !== null) {
+                        result.push(entry);
+                    }
+                });
+                return result;
+            };
+        });
+})();
+
 (function () {
     'use strict';
     angular.module('musApp').config(['$routeProvider', function ($routeProvider) {
